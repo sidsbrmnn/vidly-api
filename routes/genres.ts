27b8ecbba654ca546c716d0d210ae9aee7239746
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-11-14 19:10:13 +0530

    routes: Migrations to ts

---
+++ routes/genres.ts
@@ -0,0 +1,83 @@
+import express, { Request, Response } from 'express';
+
+import admin from '../middlewares/admin';
+import auth from '../middlewares/auth';
+
+import Genre, { validateGenre } from '../models/genre';
+import Movie from '../models/movie';
+
+const router = express.Router();
+
+router.get('/', async (req: Request, res: Response) => {
+    const genres = await Genre.find().sort('name');
+
+    res.send({ success: true, genres });
+});
+
+router.post('/', async (req: Request, res: Response) => {
+    const { error } = validateGenre(req.body);
+    if (error)
+        return res
+            .status(400)
+            .send({ success: false, message: error.details[0].message });
+
+    const genre = new Genre({ name: req.body.name });
+    await genre.save();
+
+    res.send({ success: true, genre });
+});
+
+router.put('/:id', async (req: Request, res: Response) => {
+    const { error } = validateGenre(req.body);
+    if (error)
+        return res
+            .status(400)
+            .send({ success: false, message: error.details[0].message });
+
+    const genre = await Genre.findOneAndUpdate(
+        { _id: req.params.id },
+        { name: req.body.name },
+        { new: true }
+    );
+
+    if (!genre)
+        return res.status(404).send({
+            success: false,
+            message: 'The genre with the given ID was not found'
+        });
+
+    res.send({ success: true, genre });
+});
+
+router.delete('/:id', async (req: Request, res: Response) => {
+    const genre = await Genre.findOne({ _id: req.params.id });
+    if (!genre)
+        return res.status(404).send({
+            success: false,
+            message: 'The genre with the given ID was not found'
+        });
+
+    const movies = await Movie.find({ genre: genre._id });
+    if (movies.length)
+        return res.status(400).send({
+            success: false,
+            message: 'The genre with the given ID cannot be deleted'
+        });
+
+    await genre.remove();
+
+    res.send({ success: true, genre });
+});
+
+router.get('/:id', async (req: Request, res: Response) => {
+    const genre = await Genre.findOne({ _id: req.params.id });
+    if (!genre)
+        return res.status(404).send({
+            success: false,
+            message: 'The genre with the given ID was not found'
+        });
+
+    res.send({ success: true, genre });
+});
+
+export default router;
---
+++ routes/users.ts
@@ -0,0 +1,30 @@
+import _ from 'lodash';
+import express, { Request, Response } from 'express';
+
+import User, { validateUser } from '../models/user';
+
+const router = express.Router();
+
+router.post('/', async (req: Request, res: Response) => {
+    const { error } = validateUser(req.body);
+    if (error)
+        return res
+            .status(400)
+            .send({ success: false, message: error.details[0].message });
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user)
+        return res.status(400).send({
+            success: false,
+            message: 'User with the given email already exists'
+        });
+
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+    await user.save();
+
+    const token = user.generateAuthToken();
+
+    res.send({ success: true, token });
+});
+
+export default router;
