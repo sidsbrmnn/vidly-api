f0f7addf8893776165f0ed458bd00ddb2667af51
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-08-12 23:12:10 +0530

    Remove old code

--- services/config.ts
+++
@@ -1,7 +0,0 @@
-export default function() {
-    const { MONGODB_URI, JWT_SECRET } = process.env;
-
-    if (!MONGODB_URI) throw new Error('MONGODB_URI is not defined.');
-
-    if (!JWT_SECRET) throw new Error('JWT_SECRET is not defined.');
-}
