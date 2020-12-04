bf449790f2425307ce9319e5f440e9ba5030de74
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-12-04 18:06:05 +0530

    Fix HttpError objects failing instanceof HttpError

--- src/utils/http-error.ts
+++ src/utils/http-error.ts
@@ -3,7 +3,9 @@ class HttpError extends Error {
 
     constructor(statusCode: number, message: string) {
         super(message);
+        this.name = 'HttpError';
         this.statusCode = statusCode;
+        Object.setPrototypeOf(this, HttpError.prototype);
     }
 }
 
